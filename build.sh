e2bedb2d878738314d6c5de4295d0e60143d33c4
seungha.son <linuxias@gmail.com>
2023-04-28 16:16:50 +0900

    Remove virtual environment setting
    Signed-off-by: seungha.son <linuxias@gmail.com>

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # Install environment package
 apt install -y software-properties-common build-essential zlib1g-dev libncurses5-dev \
 			   libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev \
-			   wget curl git python-is-python3 python3-distutils liblzma-dev \
-			   libbz2-dev libsqlite3-dev
+			   wget curl git python3-distutils liblzma-dev \
+			   libbz2-dev libsqlite3-dev python-is-python3
 
 # Install database using by mlflow
 apt install -y sqlite3 libsqlite3-dev
@@ -18,17 +18,5 @@ make -j16
 make install
 ln -s /usr/local/bin/python3 /usr/bin/python
 
-# Setup python virtual environment using pyenv + poetry
-apt upgrade -y
-git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
-$PYENV_ROOT/plugins/python-build/install.sh
-/usr/local/bin/python-build -v $PYTHON_VERSION $PYTHON_ROOT
-#rm -rf $PYENV_ROOT
-curl -sSL https://install.python-poetry.org | python -
-
-# Setup mlflow virtual environment
-export PATH="/root/.local/bin:$PATH"
-poetry config virtualenvs.create false
-apt remove -y python3-distro-info
-poetry install
-poetry add "mlflow==${mlflow_version}"
+# Install mlflow
+pip3 install mlflow
